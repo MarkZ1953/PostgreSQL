ab516a2d9b793a7e7ced2279a5f2b4b5756773a4
Felipe Castro <castrolopez.davidfelipe@gmail.com>
2023-02-25 11:04:32 -0500

    .

--- Funcion_fethcone.py
+++ Funcion_fethcone.py
@@ -2,7 +2,8 @@ import psycopg2
 
 class DB:
     def __init__(self) -> None:
-        pass
+        self.conexion_DB()
+        self.fetch_one()
 
     def conexion_DB(self):
         self.my_connection = psycopg2.connect(user="postgres",
@@ -10,3 +11,20 @@ class DB:
         host = "127.0.0.1",
         port = "5432",
         database = "teste_db")
+    
+    def fetch_one(self):
+        try:
+            with self.my_connection.cursor() as cursor:
+                sentencia = "SELECT * FROM persona WHERE id_persona = %s"
+                # id_persona = input("Ingrese el valor de Id_persona : ") 
+                #No es necesario convertir el valor obtenido atravez de consola en un entero, este parametro admite string
+                id_persona = 2
+                cursor.execute(sentencia,(id_persona,))
+                registros = cursor.fetchone()
+                print(registros)
+        except Exception as e:
+            print(f"Ha ocurrido un error : {e}")
+        finally:
+            self.my_connection.close()
+
+DB()

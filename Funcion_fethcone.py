ab320b8eb91c6bb7a7c73e436f2db57171aad97e
Felipe Castro <castrolopez.davidfelipe@gmail.com>
2023-02-25 10:52:13 -0500

    .

---
+++ Funcion_fethcone.py
@@ -0,0 +1,12 @@
+import psycopg2
+
+class DB:
+    def __init__(self) -> None:
+        pass
+
+    def conexion_DB(self):
+        self.my_connection = psycopg2.connect(user="postgres",
+        password="admin",
+        host = "127.0.0.1",
+        port = "5432",
+        database = "teste_db")
